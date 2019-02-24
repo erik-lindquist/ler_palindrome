bfad3dc687f4fad84be5c8024b132689c85c87c8
erik lindquist <erik.lindquist@acimacredit.com>
2019-02-24 10:31:23 -0700

    Initialize repository

---
+++ lib/ler_palindrome.rb
@@ -0,0 +1,6 @@
+require "ler_palindrome/version"
+
+module LerPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ler_palindrome/version.rb
@@ -0,0 +1,3 @@
+module LerPalindrome
+  VERSION = "0.1.0"
+end
