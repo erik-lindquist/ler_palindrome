8c4390e9ebd2268c0f6261b2dac5247b95d022d5
erik lindquist <erik.lindquist@acimacredit.com>
2019-02-24 12:00:11 -0700

    Finish working and refactored palindrome method

--- lib/ler_palindrome.rb
+++ lib/ler_palindrome.rb
@@ -1,6 +1,18 @@
 require "ler_palindrome/version"
 
-module LerPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string.
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
